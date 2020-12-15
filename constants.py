f236aac9207093cddee7b1f27f26684cc910eecd
EmilioLombardo <emilomb3@gmail.com>
2020-12-15 18:49:42 +0100

    Added falling speeds-list

--- constants.py
+++ constants.py
@@ -14,13 +14,13 @@ ROWS = 20
 fieldWidth = 300
 fieldHeight = (ROWS//COLS) * fieldWidth
 fieldPos = ( # (x, y) for top-left corner of playing field
-	(width // 2) - (fieldWidth // 2),
-	(height // 2) - (fieldHeight // 2)
-	)
+    (width // 2) - (fieldWidth // 2),
+    (height // 2) - (fieldHeight // 2)
+    )
 cellSize = fieldWidth // COLS
 
-DAS = 16 # Delayed auto-shift (in frames)
-ARR = 6 # Auto repeat rate (in frames)
+DAS = 8 # Delayed auto-shift (frames)
+ARR = 3 # Auto repeat rate (in frames/cell)
 
 UP_KEYS = [pygame.K_UP, pygame.K_w]
 DOWN_KEYS = [pygame.K_DOWN, pygame.K_s]
@@ -41,3 +41,15 @@ BLUE_GRAY = (20, 50, 70)
 
 # What colours to use for the various tetriminos. Index represents piece type
 colours = (PURPLE, BLUE, GREEN, YELLOW, RED, ORANGE, CYAN)
+
+# Falling speeds for different levels
+framesPerCell = [52, 48, 44, 40, 36, 32, 27, 21, 16, 10,
+                 9, 8, 7, 6, 5, 5, 4, 4, 3, 3,
+                 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+                 1]
+
+### 14 + 14 + 14 -- line clear animation
+### 32 -- lock delay
+### 4 -- locking animation before next spawn
+### max 31 -- freeze frames after spawn, set to 0 when any key is pressed
+### (after freeze frames comes fall delay)
