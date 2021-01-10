0b64b04c02069b0682e3da6d88da814be10f1dc0
EmilioLombardo <emilomb3@gmail.com>
2021-01-10 23:43:25 +0100

    Actually utilise reformatted sfx

--- constants.py
+++ constants.py
@@ -40,14 +40,14 @@ CONFIRM_KEYS = [pygame.K_SPACE, pygame.K_RETURN]
 PAUSE_KEYS = [pygame.K_SPACE, pygame.K_RETURN, pygame.K_TAB]
 
 # ------ Sounds ------ #
-rot_sound = pygame.mixer.Sound("sounds/rot_sound.wav")
-shift_sound = pygame.mixer.Sound("sounds/shift_sound.wav")
+rot_sound = pygame.mixer.Sound("alt_sounds/rot_sound.wav")
+shift_sound = pygame.mixer.Sound("alt_sounds/shift_sound.wav")
 shift_sound.set_volume(0.8)
-lock_sound = pygame.mixer.Sound("sounds/lock_sound.wav")
-clear_sound = pygame.mixer.Sound("sounds/clear_sound.wav")
-tetris_sound = pygame.mixer.Sound("sounds/tetris_sound.wav")
-level_up_sound = pygame.mixer.Sound("sounds/level_up_sound.wav")
-game_over_sound = pygame.mixer.Sound("sounds/game_over_sound.wav")
+lock_sound = pygame.mixer.Sound("alt_sounds/lock_sound.wav")
+clear_sound = pygame.mixer.Sound("alt_sounds/clear_sound.wav")
+tetris_sound = pygame.mixer.Sound("alt_sounds/tetris_sound.wav")
+level_up_sound = pygame.mixer.Sound("alt_sounds/level_up_sound.wav")
+game_over_sound = pygame.mixer.Sound("alt_sounds/game_over_sound.wav")
 
 # ------ Colours ------ #
 
